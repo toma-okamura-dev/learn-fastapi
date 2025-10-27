ed8cce6557eeda35bfb7a5c685acd4ef0af0a115
Toma Okamura <toma.okamura.job@gmail.com>
2025-10-27 22:04:10 +0900

    定義済みの値だけをパラメータで受け取るためにEnumクラスを作成

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class ModelName(str, Enum):  
+    alexnet = "a"
+    resnet = "b"
+    lenet = "c"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: str):
-    return {"item_id": item_id}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    return {"model_name": model_name}
